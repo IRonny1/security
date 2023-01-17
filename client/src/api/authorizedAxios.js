61db9ec437380460ea72933406161cae32780e97
TarasRospodniuk <87070317+TarasRospodniuk@users.noreply.github.com>
2023-01-17 11:51:12 +0200

    - added ability to refresh token

--- client/src/api/authorizedAxios.js
+++ client/src/api/authorizedAxios.js
@@ -1,18 +1,33 @@
 import axios from "axios";
 
-import { getJwtToken, removeJwtToken } from "../services/jwtTokenService";
+import {
+  getJwtToken,
+  removeJwtToken,
+  setJwtToken,
+} from "../services/jwtTokenService";
 
 const authorizedAxios = axios.create();
 
-authorizedAxios.interceptors.request.use(
-  (config) => {
-    config.headers.Authorization = `Bearer ${getJwtToken()}`;
+authorizedAxios.interceptors.request.use((config) => {
+  config.headers.Authorization = `Bearer ${getJwtToken()}`;
 
+  return config;
+});
+
+authorizedAxios.interceptors.response.use(
+  (config) => {
     return config;
   },
   (err) => {
-    if (err.response.status === 401 || err.response.status === 403) {
-      removeJwtToken();
+    if (err.response.status === 401) {
+      try {
+        axios.post("/api/user/refresh").then((res) => {
+          setJwtToken(res.data.accessToken);
+          return axios.request(err.config);
+        });
+      } catch (e) {
+        removeJwtToken();
+      }
     }
   }
 );
